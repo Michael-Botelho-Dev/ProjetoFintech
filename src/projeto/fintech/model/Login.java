cfa1b8327a254ee5428c5575ab4c115831c211c8
Michael-Botelho-Dev <mbotelho2303@gmail.com>
2025-08-24 19:50:39 -0300

    fix: Login.java

--- src/projeto/fintech/model/Login.java
+++ src/projeto/fintech/model/Login.java
@@ -3,16 +3,21 @@ package projeto.fintech.model;
 public class Login {
 
     private String username;
-
     private String password;
+    boolean logado = false;
+    private String email2;
+    private String password2;
 
-    public Login(String username, String password){
+    public Login() {
+    }
+
+    public Login(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    public void doLogin(){
-        System.out.println("Realizando login para o usuário:" + username);
+    public void fazerLogin() {
+        boolean logado = true;
+        System.out.println("Login Realizado com Sucesso!");
     }
-
 }
